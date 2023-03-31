11c2e9dd5de7dc88ab6253987be11bde428f5d09
Totyxd <tobiaslevy1409@gmail.com>
2023-03-31 19:34:39 -0300

    Full hash tables practice

---
+++ hash-tables/hash-tables-implementation/phases/01-implementation.js
@@ -0,0 +1,161 @@
+class KeyValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+  };
+};
+
+class HashTable { // get O(1), set O(1), deleteKey O(1)
+
+  constructor(numBuckets = 8) {
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets);
+    this.count = 0;
+
+    for (let index = 0; index < this.data.length; index++) {
+      this.data[index] = null;
+    };
+  };
+
+  hash(key) {
+    let hashValue = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hashValue += key.charCodeAt(i);
+    };
+
+    return hashValue;
+  };
+
+  hashMod(key) {
+    // Get index after hashing
+    return this.hash(key) % this.capacity;
+  };
+
+
+  insert(key, value) {
+    if (this.count / this.capacity >= 0.7) this.resize();
+    const bucket = this.hashMod(key);
+
+    if (!this.data[bucket]) {
+      this.data[bucket] = new KeyValuePair(key, value);
+      this.count++;
+      return;
+    };
+
+    if (this.data[bucket].key === key) {
+      this.data[bucket].value = value;
+      return;
+    };
+
+    if (this.data[bucket].next) {  //Check for possible overwriting.
+
+      let node = this.data[bucket].next;
+
+      while (node) {
+        if (node.key === key) {                   //O(n) section only in same bucket cases. Otherwise, O(1).
+          node.value = value;
+          return;
+        };
+
+        node = node.next;
+      };
+
+    };
+
+    const oldHead = this.data[bucket];
+    this.data[bucket] = new KeyValuePair(key, value);
+    this.data[bucket].next = oldHead;
+    this.count++;
+  };
+
+
+  read(key) {
+    const index = this.hashMod(key);
+    const bucket = this.data[index];
+
+    if (!bucket) return;
+    if (bucket && bucket.key === key) return bucket.value;
+    if (bucket) {
+
+      let node = bucket.next;
+
+      while (node) {
+        if (node.key === key) {
+          return node.value;
+        };
+
+        node = node.next;
+      };
+    };
+  };
+
+
+  resize() {
+    this.capacity *= 2;
+    let newHashTab = new HashTable(this.capacity);
+
+    for (let index = 0; index < this.data.length; index++) {
+      if (this.data[index]) {
+
+          if (this.data[index].next) {
+
+            let node = this.data[index];
+
+            while (node) {
+                newHashTab.insert(node.key, node.value);
+                node = node.next;
+            };
+
+          } else {
+            newHashTab.insert(this.data[index].key, this.data[index].value);
+          };
+      };
+    };
+
+    for (let index = 0; index < newHashTab.data.length; index++) {
+      this.data[index] = newHashTab.data[index];
+    };
+  };
+
+
+  delete(key) {
+    const idx = this.hashMod(key);
+    const bucket = this.data[idx];
+
+    if (!bucket) return "Key not found";
+
+    if (bucket.key === key) {
+      this.data[idx] = bucket.next;
+      this.count--;
+      return;
+    };
+
+    let node = bucket.next;
+    let prevNode = bucket;
+    while (node) {
+      if (node.key === key) {
+        prevNode.next = node.next;
+        this.count--;
+        return;
+      }
+      prevNode = node;
+      node = node.next;
+    };
+
+    return "Key not found";
+  };
+};
+
+let hashTable = new HashTable(2);
+
+hashTable.insert("key1", "value1");
+hashTable.insert("key2", "value2");
+hashTable.insert("key3", "value3");
+hashTable.resize();
+console.log(hashTable);
+
+
+
+module.exports = HashTable;
---
+++ hash-tables/hash-tables-implementation/phases/02-practice-problems.js
@@ -0,0 +1,101 @@
+const HashTable = require("./01-implementation");
+
+function anagrams(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  let str1Table = new HashTable(str1.length);
+  for (let index = 0; index < str1.length; index++) {
+    const char = str1[index];
+    str1Table.insert(`key-${char}`, true);
+  };
+
+  for (let index = 0; index < str2.length; index++) {
+    const char = str2[index];
+    if (!str1Table.read(`key-${char}`)) {
+      return false;
+    };
+  };
+
+  return true;
+};
+
+
+function commonElements(arr1, arr2) {
+  const newArr = [];
+
+  let arr1Table = new HashTable(arr1.length);
+  for (let index = 0; index < arr1.length; index++) {
+    const el = arr1[index];
+    arr1Table.insert(`key-${el}`, true);
+  };
+
+  for (let index = 0; index < arr2.length; index++) {
+    const el = arr2[index];
+    if (arr1Table.read(`key-${el}`)) {
+      newArr.push(el);
+    };
+  };
+
+  return newArr
+};
+
+
+function duplicate(arr) {
+  let arrTable = new HashTable(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+    const count = arrTable.read(`key-${el}`) || 0;
+    if (count === 1) {
+      return el;
+    };
+    arrTable.insert(`key-${el}`, count + 1);
+  };
+};
+
+
+function twoSum(nums, target) {
+  const seen = new HashTable(nums.length);
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (seen.read(`key-${complement}`)) {
+      return true;
+    };
+    seen.insert(`key-${nums[i]}`, nums[i]);
+  };
+
+  return false;
+};
+
+
+
+function wordPattern(pattern, words) {
+  if (pattern.length !== words.length) {
+    return false;
+  }
+
+  const map = {};
+  const usedWords = new Set();
+
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+    const word = words[i];
+
+    if (!map[char] && !usedWords.has(word)) {
+      map[char] = word;
+      usedWords.add(word);
+    } else if (map[char] !== word) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+
+
+
+
+
+
+module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
