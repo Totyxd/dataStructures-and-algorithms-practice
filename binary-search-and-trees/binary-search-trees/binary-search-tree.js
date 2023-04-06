5f619b87d01532d5fd89d67d1cc892682ed9a768
Totyxd <tobiaslevy1409@gmail.com>
2023-04-06 17:59:14 -0300

    BST class implementation. O(log n) compl

---
+++ binary-search-and-trees/binary-search-trees/binary-search-tree.js
@@ -0,0 +1,112 @@
+// Do not change this
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  };
+};
+
+class BinarySearchTree {
+
+  constructor(root = null) {
+    this.root = root;
+  };
+
+  insert(val, node = this.root) {
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    };
+
+    if (!node.left && node.val > val) {
+      node.left = new TreeNode(val);
+      return;
+    };
+
+    if (!node.right && node.val < val) {
+      node.right = new TreeNode(val);
+      return;
+    };
+
+    if (val < node.val) {           //Not considering if val is equal for simplification.
+      this.insert(val, node.left);
+    } else {
+      this.insert(val, node.right);
+    };
+  };
+
+  search(target, node = this.root) {
+    if (!node) return false;
+
+    if (node.val === target) return true;
+
+    if (target < node.val) {
+      return this.search(target, node.left);
+    } else {
+      return this.search(target, node.right);
+    };
+  };
+
+  preOrderTraversal(node = this.root) {
+    if (!node) return;
+
+    console.log(node.val);
+
+    this.preOrderTraversal(node.left);
+    this.preOrderTraversal(node.right);
+  };
+
+  inOrderTraversal(node = this.root) {
+    if (!node) return;
+
+    this.inOrderTraversal(node.left);
+    console.log(node.val);
+    this.inOrderTraversal(node.right);
+  };
+
+  postOrderTraversal(node = this.root) {
+    if (!node) return;
+
+    this.postOrderTraversal(node.left);
+    this.postOrderTraversal(node.right);
+
+    console.log(node.val);
+  };
+
+  breadthFirstTraversal() {
+    let queue = [this.root];
+
+    while(queue.length > 0) {
+      const shifted = queue.shift();
+      console.log(shifted.val);
+      if (shifted.left) queue.push(shifted.left);
+      if (shifted.right) queue.push(shifted.right);
+    };
+  };
+
+  depthFirstTraversal() {
+    const stack = [this.root];
+
+    while (stack.length > 0) {
+
+      let node = stack.pop();
+      console.log(node.val);
+
+      if (node.left) stack.push(node.left);
+      if (node.right) stack.push(node.right);
+    };
+  };
+};
+
+const BST = new BinarySearchTree();
+BST.insert(10);
+BST.insert(5);
+BST.insert(15);
+BST.insert(12);
+BST.insert(13);
+BST.insert(8);
+console.log(BST);
+console.log(BST.search(15));
+
+module.exports = { BinarySearchTree, TreeNode };
